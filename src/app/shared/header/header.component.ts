79ee6554880af2e3d01de419b4b7c98bff50b939
erbert-gadelha <ebgr@cin.ufpe.br>
2025-01-05 15:12:52 -0300

    feat: desing do componente header atualizado

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -30,7 +30,7 @@ export class HeaderComponent implements OnInit {
       last: 0,
       delta: 0,
       height: 64,
-      clamp: 8-64
+      clamp: 4-64
     };
   }
 
@@ -45,17 +45,20 @@ export class HeaderComponent implements OnInit {
     if(((diff > 0) && this.scroll.delta == 0) || ((diff < 0) && this.scroll.delta == this.scroll.clamp)) {
       this.scroll.last = window.scrollY;
         return;      
-    }    
+    }
+
+
+
     this.scroll.delta = this._clamp (this.scroll.clamp, this.scroll.delta + diff, 0);
     this.scroll.last = window.scrollY;  
     
     this.root.style.setProperty('--navbar-delta', `${this.scroll.delta}px`);
-    if(this.scroll.last-window.scrollY < 0 || this.scroll.delta == this.scroll.clamp) {
+    /*if(this.scroll.last-window.scrollY < 0 || this.scroll.delta == this.scroll.clamp) {
       this.root.style.setProperty('--navbar-opacity', 1);
     } else {
       const ratio = this.scroll.delta/this.scroll.height;
       this.root.style.setProperty('--navbar-opacity', 1+(2*ratio/3));
-    }
+    }*/
   };
 
 
